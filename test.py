1b254c546f67478d5e38612c8a2d43b9d42b25dd
Hoyinl2333 <1974562160@qq.com>
2025-05-25 16:22:20 +0800

    init

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-envs = [
-    "dataStructureProject", "first", "imgOperation", "kaiwu",
-    "llm", "mathorcupPython", "pythonProject2", "test"
-]
-
-for env in envs:
-    os.system(f"conda env remove -n {env} -y")
